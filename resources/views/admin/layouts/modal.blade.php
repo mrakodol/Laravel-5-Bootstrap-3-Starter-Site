e2499de37bd2c517907fcdade5191b705754de56
Stojan Kukrika <mrakodol@gmail.com>
2015-02-24 15:38:21 +0100

    update modal with showing errors

--- resources/views/admin/layouts/modal.blade.php
+++ resources/views/admin/layouts/modal.blade.php
@@ -91,12 +91,22 @@
 							  type : form.attr('method'),
 							  url : form.attr('action'),
 							  data : form.serialize()
-							  }).complete(function() {
+							  }).success(function() {
 								  setTimeout(function() {
 									  parent.$.colorbox.close();
 									  window.parent.location.reload();
 									  }, 10);
-							}).fail(function() {});
+							}).fail(function(jqXHR, textStatus, errorThrown) {
+			                    // Optionally alert the user of an error here...
+			                    var textResponse = jqXHR.responseText;
+			                    var alertText = "One of the following conditions is not met:\n\n";
+			                    var jsonResponse = jQuery.parseJSON(textResponse);
+
+			                    $.each(jsonResponse, function(n, elem) {
+			                        alertText = alertText + elem + "\n";
+			                    });
+			                    alert(alertText);
+			                });
 						}
 					else{
 						var formData = new FormData(this);
@@ -108,14 +118,25 @@
 							  contentType: false,
 							  cache: false,
 							  processData:false
-						}).complete(function() {
+						}).success(function() {
 							  setTimeout(function() {
 								  parent.$.colorbox.close();
 								  window.parent.location.reload();
 								  }, 10);
 
-						}).fail(function() {});
-						};
+						}).fail(function(jqXHR, textStatus, errorThrown) {
+		                    // Optionally alert the user of an error here...
+		                    var textResponse = jqXHR.responseText;
+		                    var alertText = "One of the following conditions is not met:\n\n";
+		                    var jsonResponse = jQuery.parseJSON(textResponse);
+
+		                    $.each(jsonResponse, function(n, elem) {
+		                        alertText = alertText + elem + "\n";
+		                    });
+
+		                    alert(alertText);
+		                });
+					};
 				});
 
 				$('.close_popup').click(function() {
