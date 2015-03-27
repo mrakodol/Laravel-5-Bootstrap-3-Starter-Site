af06595b4072b6b3d47085df34b32954f3195a60
Ciarán O'Mara <kezkankrayon@gmail.com>
2015-03-27 18:28:29 +1100

    Minor style fixes and tweaks.
    Fix sticky footer.
    Tweaked footer styles.
    Made top nav fixed.

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default navbar-inverse navbar-static-top">
+<nav class="navbar navbar-default navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
