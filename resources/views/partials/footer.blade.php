169b6c73b849e5eb5d29bc819b22e35e9d530d70
stojankukrika <stojan.kukrika@gmail.com>
2015-07-16 10:09:06 +0200

    fixed footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,6 +3,6 @@
         <p class="text-muted credit"><span style="text-align: left; float: left">&copy; 2015 <a href="#">Laravel
                     5 Starter Site</a></span> <span class="hidden-phone"
                                                     style="text-align: right; float: right">Powered by: <a
-                        href="http://laravel.com/" alt="Laravel 5.1">Laravel 5.1</a></span></p>
+                        href="http://laravel.com/" alt="Laravel 5.0">Laravel 5.0</a></span></p>
     </div>
 </div>
