c9577f5685d115bd0da886a717fde287fc0a12e9
stojankukrika <stojan.kukrika@gmail.com>
2015-09-02 14:55:38 +0200

    show photo in article

--- resources/views/article/view.blade.php
+++ resources/views/article/view.blade.php
@@ -3,14 +3,18 @@
 @section('title') {!!  $article->title !!} :: @parent @stop
 
 @section('meta_author')
-<meta name="author" content="{!! $article->author->username !!}" />
+    <meta name="author" content="{!! $article->author->username !!}"/>
 @stop
 {{-- Content --}}
 @section('content')
-<h3>{{ $article->title }}</h3>
-<p>{!! $article->introduction !!}</p>
-<p>{!! $article->content !!}</p>
-<div>
-	<span class="badge badge-info">Posted {!!  $article->created_at !!} </span>
-</div>
+    <h3>{{ $article->title }}</h3>
+    <p>{!! $article->introduction !!}</p>
+    @if($article->picture!="")
+        <img alt="{{$article->picture}}"
+             src="{!! URL::to('appfiles/article/'.$article->id.'/'.$article->picture) !!}"/>
+    @endif
+    <p>{!! $article->content !!}</p>
+    <div>
+        <span class="badge badge-info">Posted {!!  $article->created_at !!} </span>
+    </div>
 @stop
--- resources/views/photo/view_album.blade.php
+++ resources/views/photo/view_album.blade.php
@@ -1,13 +1,13 @@
-@extends('app') @section('title') {{{
-$photo_album->name }}} :: @parent @stop @section('content')
-<h3>{{{ $photo_album->name }}}</h3>
+@extends('app') @section('title') {!!
+$photo_album->name !!} :: @parent @stop @section('content')
+<h3>{!! $photo_album->name !!}</h3>
 
 <div id="mygallery">
 	@foreach($photos as $item) 
 	<a
-		href="{{{'../../appfiles/photoalbum/'.$photo_album->folder_id.'/'.$item->filename }}}"
+		href="{!!'../../appfiles/photoalbum/'.$photo_album->folder_id.'/'.$item->filename !!}"
 		data-lightbox="roadtrip"> <img alt="{{$item->name}}"
-		src="{{{'../../appfiles/photoalbum/'.$photo_album->folder_id.'/thumbs/'.$item->filename }}}" />
+		src="{!!'../../appfiles/photoalbum/'.$photo_album->folder_id.'/thumbs/'.$item->filename !!}" />
 	</a> 
 	@endforeach
 </div>
