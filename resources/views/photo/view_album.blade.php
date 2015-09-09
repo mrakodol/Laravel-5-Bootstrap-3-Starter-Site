6ef6597d5009525f6abbba3d037ab9ecc29af9de
Stojan Kukrika <stojan.kukrika@gmail.com>
2015-09-09 11:22:47 +0200

    small fix in photo frontend

--- resources/views/photo/view_album.blade.php
+++ resources/views/photo/view_album.blade.php
@@ -1,13 +1,13 @@
-@extends('app') @section('title') {!!
-$photo_album->name !!} :: @parent @stop @section('content')
+@extends('layouts.app')
+@section('title')
+	{!! $photo_album->name !!} :: @parent @stop @section('content')
 <h3>{!! $photo_album->name !!}</h3>
-
 <div id="mygallery">
 	@foreach($photos as $item) 
 	<a
-		href="{!!'../../appfiles/photoalbum/'.$photo_album->folder_id.'/'.$item->filename !!}"
+		href="{!! URL::to('appfiles/photoalbum/'.$photo_album->folder_id.'/thumbs/'.$item->filename) !!}"
 		data-lightbox="roadtrip"> <img alt="{{$item->name}}"
-		src="{!!'../../appfiles/photoalbum/'.$photo_album->folder_id.'/thumbs/'.$item->filename !!}" />
+		src="{!!URL::to('appfiles/photoalbum/'.$photo_album->folder_id.'/'.$item->filename) !!}" />
 	</a> 
 	@endforeach
 </div>
